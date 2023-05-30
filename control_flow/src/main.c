d44783929e30b6d24e4d5bc0a47aa54e23b61906
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-05-30 19:33:14 +0530

    Control flow complete.

---
+++ control_flow/src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+  int age = 18;
+  if (age >= 21) {
+    printf("You are legal to drink\n");
+  } else if (age >= 18) {
+    printf("You are legal to drive\n");
+  } else {
+    printf("You are minor\n");
+  }
+  char ans = 'y';
+  switch (ans) {
+  case 'y':
+    printf("You answered yes\n");
+    break;
+  case 'n':
+    printf("You answered no\n");
+    break;
+  default:
+    printf("Not an answer\n");
+  }
+}
