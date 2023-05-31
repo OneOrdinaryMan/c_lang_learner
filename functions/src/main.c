8d772025870fbd0a67337e5ad12cf0c1707f8088
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-05-31 09:15:53 +0530

    function prototyping added.

--- functions/src/main.c
+++ functions/src/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 void hello() { printf("Hello world"); }
 int square(int num) { return num * num; }
+int max(int, int);
 int main() {
   hello();
   int num = 5;
   int sqr = square(num);
   printf("%d\n", sqr);
+  int max_integer = max(10, 7);
+  printf("%d\n", max_integer);
   return 0;
 }
+int max(int num1, int num2) { return (num1 > num2) ? num1 : num2; }
